02d3ab90c95f5d94d83cbed05ee33d79267f79cf
Daniel Bruder <daniel.bruder+git@gmail.com>
2013-07-04 09:48:13 +0200

    add main_module to dist.ini

--- lib/Acme/Schlong.pm
+++ lib/Acme/Schlong.pm
@@ -304,17 +304,3 @@ Send report to website
 =back
 
 =cut
-
-=head1 AUTHOR
-
-Daniel B. <dbr@cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is Copyright (c) 2012 by Daniel B..
-
-This is free software, licensed under:
-
-  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE, Version 2, December 2004
-
-=cut
